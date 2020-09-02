b495e75705a280020e594372cdee70d8ee415c2e
AlexandruDumitru94 <alexandru.dumitru94@gmail.com>
2020-09-02 12:24:41 +0300

    Initial Structure v 0.3

--- script.js
+++ script.js
@@ -3,15 +3,20 @@
 $(function() {
     $.jInvertScroll([ '.scroll' ], 
     {
-        // height: 6000,                   // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
+        
+         // height: 6000,                   // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
          onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
              console.log(percent);
+            
+             
          }
      });
 });
 
+
 function scrollToPage(pageNumber) {
     var bodyWidth =document.getElementsByClassName("section--fourth")[0].scrollWidth;
+    console.log(bodyWidth);
     var screenWidth = document.getElementsByClassName("page")[0].scrollWidth;
     var screenHeight = document.getElementsByClassName("page")[0].scrollHeight;
     var quarterSizeDif = (screenWidth - screenHeight) / 4;
@@ -20,4 +25,21 @@ function scrollToPage(pageNumber) {
         pageScroll += (quarterSizeDif + (bodyWidth - screenWidth) / 4) * pageNumber;
     }
     window.scrollTo(0, pageScroll);
+    console.log(screenWidth);
+    console.log(screenHeight);
 }
+
+function gallerySize() {
+            var camera = document.getElementById("camera");
+
+            var cameraWidth = camera.clientWidth;
+            var cameraHeight = camera.clientHeight;
+
+            var y = cameraWidth + "px";
+            var x = cameraHeight + "px";
+        
+            var container = document.getElementsByClassName("camera-container")[0];
+
+            container.style.height = x;
+            container.style.width = y;
+        }
