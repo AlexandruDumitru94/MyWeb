c72aa3cd521dc53668f32a46eefb82a586057d94
AlexandruDumitru94 <alexandru.dumitru94@gmail.com>
2020-10-13 17:07:17 +0300

    Final Test

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ innerSparkles = document.getElementsByClassName("inner-sparkle");
 
 headShot = document.getElementById("head-shot");
 headShotWidth = window.getComputedStyle(headShot).getPropertyValue("width");
+console.log(headShotWidth);
 headShotHeight = window.getComputedStyle(headShot).getPropertyValue("height");
 welcomeText = document.getElementById("welcome");
 welcomeTextLeft = window.getComputedStyle(welcomeText).getPropertyValue("left");
@@ -44,7 +45,7 @@ welcomeTextLeft = window.getComputedStyle(welcomeText).getPropertyValue("left");
 startTimeline = gsap.timeline({defaults: {duration: 1}});
 
         startTimeline.fromTo(".greeting__text", {opacity: 0, top: "-=100", visibility: "hidden"}, {opacity: 1, top: 0, visibility: "visible", stagger: 1});
-        startTimeline.fromTo("#head-shot", {width: 0, height: 0, visibility: "hidden"}, {width: headShotWidth, height: headShotHeight, visibility: "visible"}, 3.5);
+        startTimeline.fromTo("#head-shot", {width: 0, height: 0, visibility: "hidden"}, {width: headShotWidth, height: headShotHeight, visibility: "visible"}, "+=0.5");
         startTimeline.fromTo("#welcome", {opacity: 0, left: "-50%", visibility: "hidden"}, {opacity: 1, left: welcomeTextLeft, visibility: "visible"}, "-=1");
 
 if(window.readyState == 'loading') {
